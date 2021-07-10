982c391a1d04985841907f86aab87cd03335009b
JerryLubuntu <runanye@usc.edu>
2021-07-10 16:40:45 +0800

    clean up

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -4,54 +4,75 @@
 #include <fstream>
 #include <regex>
 #include <string>
+#include <unordered_map>
 
-namespace LinuxParser {
-// Paths
-const std::string kProcDirectory{"/proc/"};
-const std::string kCmdlineFilename{"/cmdline"};
-const std::string kCpuinfoFilename{"/cpuinfo"};
-const std::string kStatusFilename{"/status"};
-const std::string kStatFilename{"/stat"};
-const std::string kUptimeFilename{"/uptime"};
-const std::string kMeminfoFilename{"/meminfo"};
-const std::string kVersionFilename{"/version"};
-const std::string kOSPath{"/etc/os-release"};
-const std::string kPasswordPath{"/etc/passwd"};
+namespace LinuxParser 
+{
+    // Paths
+    const std::string kProcDirectory{ "/proc/" };
+    const std::string kCmdlineFilename{ "/cmdline" };
+    const std::string kCpuinfoFilename{ "/cpuinfo" };
+    const std::string kStatusFilename{ "/status" };
+    const std::string kStatFilename{ "/stat" };
+    const std::string kUptimeFilename{ "/uptime" };
+    const std::string kMeminfoFilename{ "/meminfo" };
+    const std::string kVersionFilename{ "/version" };
+    const std::string kOSPath{ "/etc/os-release" };
+    const std::string kPasswordPath{ "/etc/passwd" };
 
-// System
-float MemoryUtilization();
-long UpTime();
-std::vector<int> Pids();
-int TotalProcesses();
-int RunningProcesses();
-std::string OperatingSystem();
-std::string Kernel();
+    // System
+    float MemoryUtilization();
+    long UpTime();
+    std::vector<int> Pids();
+    int TotalProcesses();
+    int RunningProcesses();
+    std::string OperatingSystem();
+    std::string Kernel();
 
-// CPU
-enum CPUStates {
-  kUser_ = 0,
-  kNice_,
-  kSystem_,
-  kIdle_,
-  kIOwait_,
-  kIRQ_,
-  kSoftIRQ_,
-  kSteal_,
-  kGuest_,
-  kGuestNice_
-};
-std::vector<std::string> CpuUtilization();
-long Jiffies();
-long ActiveJiffies();
-long ActiveJiffies(int pid);
-long IdleJiffies();
+    // CPU
+    enum CPUStates {
+        kUser_ = 0,
+        kNice_,
+        kSystem_,
+        kIdle_,
+        kIOwait_,
+        kIRQ_,
+        kSoftIRQ_,
+        kSteal_,
+        kGuest_,
+        kGuestNice_
+    };
+    std::vector<std::string> CpuUtilization();
+    long Jiffies();
+    long ActiveJiffies();
+    long ActiveJiffies(int pid);
+    long IdleJiffies();
 
-// Processes
-std::string Command(int pid);
-std::string Ram(int pid);
-std::string Uid(int pid);
-std::string User(int pid);
-long int UpTime(int pid);
+    // Processes
+    std::string Command(int pid);
+    std::string Ram(int pid);
+    std::string Uid(int pid);
+    std::string User(int pid);
+    long int UpTime(int pid);
+
+    
+
+    // CPU Utility
+    // long cpuUser;
+    // long cpuNice;
+    // long cpuSystem;
+    // long cpuIdle;
+    // long cpuIowait;
+    // long cpuIrq;
+    // long cpuSoftirq;
+
+    // long cpuPreUser{0};
+    // long cpuPreNice{0};
+    // long cpuPreSystem{0};
+    // long cpuPreIdle{0};
+    // long cpuPreIowait{0};
+    // long cpuPreIrq{0};
+    // long cpuPreSoftirq{0};
 };  // namespace LinuxParser
 
 #endif
